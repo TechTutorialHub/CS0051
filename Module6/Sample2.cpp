37dd8c40d62351eca2cca9afdf822f5f258eef84
TechTutorialsHub! <techtutorialshubtalks@gmail.com>
2025-03-02 19:57:18 +0800

    Update Sample2.cpp

--- Module6/Sample2.cpp
+++ Module6/Sample2.cpp
@@ -1,83 +1,33 @@
 #include <iostream>
-#include <fstream>
 #include <cstdlib>
 #include <ctime>
 #include <openacc.h>
 
 using namespace std;
 
-void generateDataFile(const string &filename, int size) {
-    ofstream outFile(filename);
+int main() {
+    const int N = 10;
+    int arr[N];
+
     srand(time(0));
 
-    if (!outFile) {
-        cerr << "Error opening file for writing!" << endl;
-        return;
+    // Initialize array with random values
+    for (int i = 0; i < N; i++) {
+        arr[i] = rand() % 100;
     }
 
-    for (int i = 0; i < size; i++) {
-        outFile << (rand() % 100) << endl;
-    }
-
-    outFile.close();
-}
-
-void readDataFile(const string &filename, int arr[], int size) {
-    ifstream inFile(filename);
-
-    if (!inFile) {
-        cerr << "Error opening file for reading!" << endl;
-        return;
-    }
-
-    for (int i = 0; i < size; i++) {
-        inFile >> arr[i];
-    }
-
-    inFile.close();
-}
-
-void writeDataFile(const string &filename, int arr[], int size) {
-    ofstream outFile(filename);
-
-    if (!outFile) {
-        cerr << "Error opening file for writing!" << endl;
-        return;
-    }
-
-    for (int i = 0; i < size; i++) {
-        outFile << arr[i] << endl;
-    }
-
-    outFile.close();
-}
-
-int main() {
-    int N;
-    cout << "Enter the number of data points: ";
-    cin >> N;
-
-    const string inputFile = "data.txt";
-    const string outputFile = "output.txt";
-    int *arr = new int[N];
-
-    generateDataFile(inputFile, N);
-    readDataFile(inputFile, arr, N);
-
-    // OpenACC parallelization
+    // Compute square of each element in parallel using OpenACC
     #pragma acc kernels
     for (int i = 0; i < N; i++) {
-        arr[i] *= arr[i];
+        arr[i] *= arr[i]; // Squaring each element
     }
 
-    writeDataFile(outputFile, arr, N);
-
-    cout << "Processed Data (Squared Values): ";
+    // Display output
+    cout << "Squared Array: ";
     for (int i = 0; i < N; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
 
-    delete[] arr;
     return 0;
 }
