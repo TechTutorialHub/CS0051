f34d224799adba174d09783491128b932f17bf2e
TechTutorialsHub! <techtutorialshubtalks@gmail.com>
2025-03-07 10:19:20 +0800

    Update ex2.cpp

--- Module6/ex2.cpp
+++ Module6/ex2.cpp
@@ -16,7 +16,7 @@ int main(){
     for (int i = 0; i<N; i++){
         
         C[i] = A[i]+B[i];
-        cout << "Iteration " <<i<< " : " << A[i] << " + " << B[i] " = " <<C[i]<<endl;
+        cout << "Iteration " <<i<< " : " << A[i] << " + " << B[i] << " = " <<C[i]<<endl;
         
     }
 
