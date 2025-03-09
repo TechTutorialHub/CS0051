8ccb8e074b1de24672efd88b6bec576335b690ef
TechTutorialsHub! <techtutorialshubtalks@gmail.com>
2025-03-09 14:34:51 +0800

    Update Sample7.cpp

--- Module6/Sample7.cpp
+++ Module6/Sample7.cpp
@@ -1,22 +1,19 @@
-//Communication	
-//Prefix Sum	
-//Synchronizes shared data
-
-
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <omp.h>
+#include <openacc.h>
 
 using namespace std;
 
 const int N = 8; // Array Size
 
+// Function to generate a random array
 void generateArray(int arr[N]) {
     for (int i = 0; i < N; i++)
-        arr[i] = rand() % 10;
+        arr[i] = rand() % 10;  // Random values from 0-9
 }
 
+// Function to print an array
 void printArray(const string &name, int arr[N]) {
     cout << name << ": ";
     for (int i = 0; i < N; i++)
@@ -28,15 +25,27 @@ int main() {
     srand(time(0));
 
     int arr[N], prefixSum[N];
+
     generateArray(arr);
     printArray("Original Array", arr);
 
-    // Parallel Prefix Sum
+    // Copy the initial value to start prefix sum calculation
     prefixSum[0] = arr[0];
 
-    #pragma omp parallel for
-    for (int i = 1; i < N; i++)
-        prefixSum[i] = prefixSum[i - 1] + arr[i];
+    // OpenACC Parallel Prefix Sum using Data Synchronization
+    #pragma acc data copyin(arr) copyout(prefixSum)
+    {
+        for (int i = 1; i < N; i++) {
+            #pragma acc parallel loop
+            for (int j = i; j < N; j++) {
+                prefixSum[j] += arr[i - 1];
+
+                // Ensure synchronization between iterations
+                #pragma acc atomic
+                cout << "Iteration " << j << ": prefixSum[" << j << "] = " << prefixSum[j] << endl;
+            }
+        }
+    }
 
     printArray("Prefix Sum", prefixSum);
 
