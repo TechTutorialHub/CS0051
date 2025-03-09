2e40df2095245f62545f9cdcc406fc934b43c841
TechTutorialsHub! <techtutorialshubtalks@gmail.com>
2025-03-09 14:31:11 +0800

    Update Sample8.cpp

--- Module6/Sample8.cpp
+++ Module6/Sample8.cpp
@@ -1,23 +1,19 @@
-
-//Agglomeration	
-//Merged Sum & Product	
-//Reduces loop overhead
-
-
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <omp.h>
+#include <openacc.h>
 
 using namespace std;
 
 const int N = 8; // Array Size
 
+// Function to generate an array with random values
 void generateArray(int arr[N]) {
     for (int i = 0; i < N; i++)
-        arr[i] = rand() % 10 + 1;
+        arr[i] = rand() % 10 + 1; // Random numbers between 1-10
 }
 
+// Function to print the array
 void printArray(const string &name, int arr[N]) {
     cout << name << ": ";
     for (int i = 0; i < N; i++)
@@ -34,15 +30,21 @@ int main() {
 
     int sum = 0, product = 1;
 
-    // Parallel computation (Agglomeration)
-    #pragma omp parallel for reduction(+:sum) reduction(*:product)
+    // OpenACC Parallel Computation (Agglomeration: Sum & Product Together)
+    #pragma acc parallel loop reduction(+:sum) reduction(*:product)
     for (int i = 0; i < N; i++) {
         sum += arr[i];
         product *= arr[i];
+
+        // Print iteration output
+        #pragma acc atomic
+        cout << "Iteration " << i << ": sum += " << arr[i] 
+             << ", product *= " << arr[i] << " --> Partial Sum: " 
+             << sum << ", Partial Product: " << product << endl;
     }
 
-    cout << "Sum: " << sum << endl;
-    cout << "Product: " << product << endl;
+    cout << "Final Sum: " << sum << endl;
+    cout << "Final Product: " << product << endl;
 
     return 0;
 }
